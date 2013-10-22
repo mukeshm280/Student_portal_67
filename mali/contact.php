68776448fdd7ce6c35fa23a746094346a141e979
205112067 <mukeshm280@gmail.com>
2013-10-22 06:14:57 +0530

    Done By :Mukesh Mali 67

---
+++ mali/contact.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+  <title>Welcome to Student Portal</title>
+</head>
+
+<body bgcolor="#C8C8C8">
+
+<div align="right">
+   <form action="home.php" method="post">
+      
+      Username :<input type="text" name="username"/>
+	  Password :
+	  <input type="text" name="pswd"/>
+	  <input type="submit" value="Login" name="Login"/></br>
+  </form>
+  <img src="img/logo_new.jpg" height="265" width="1492"/>
+</div>
+<a href="registration.php"><b> Home</b></a>
+&nbsp;<a href="gallery.php"><b>Gallery</b></a>
+&nbsp;<a href="registration.php"><b>Result</b></a>
+&nbsp;<a href="event.php"><b>Event</b></a>
+&nbsp;<a href="contact.php"><b>Contacts</b></a>
+&nbsp;<h2>Contacts</h2>
+<center>
+National Institute of Technology</br>
+
+Tanjore Main Road,</br>
+National Highway 67,</br>
+Tiruchirappalli - 620015,</br>
+Tamil Nadu,</br>
+India.</br>
+
+Telephone : </br>
+
+INSTITUTE : (0431) 2503000 / 2504000 (Each 4-digit extension can be reached directly by adding the prefix number (0431) 250 - - - - to the respective numbers)</br>
+HOSTEL : BOYS : (0431) 2504186, 2504192</br>
+GIRLS : (0431) 2504392</br>
+Fax : 91-431-2500133</br>
+Website : http://www.nitt.edu</center>
+<footer>
+  <p>Posted by: Mukesh Mali</p>
+  <p>Contact information: <a href="contact.php">205112067@nitt.edu</a>.</p>
+<img src="img/ff.png" width="1499"></footer>
+
+</body>
+</html>
---
+++ malii/event.php
@@ -0,0 +1,132 @@
+<html>
+<head>
+  <title>Welcome to Student Portal</title>
+</head>
+
+<body bgcolor="#C8C8C8">
+
+<div align="right">
+   <form action="home.php" method="post">
+      
+      Username :<input type="text" name="username"/>
+	  Password :
+	  <input type="text" name="pswd"/>
+	  <input type="submit" value="Login" name="Login"/></br>
+  </form>
+</div>
+
+<img src="img/logo_new.jpg" height="265" width="1492"/>
+<a href="registration.php"><b> Home</b></a>
+&nbsp;<a href="gallery.php"><b>Gallery</b></a>
+&nbsp;<a href="registration.php"><b>Result</b></a>
+&nbsp;<a href="registration.php"><b>Event</b></a>
+&nbsp;<a href="contact.php"><b>Contacts</b></a>
+&nbsp;
+
+<center><h1>EVENTS TO WATCHOUT FOR</h1></center>
+
+<table bordercolor="#0000CC" align="center">
+   <tr bgcolor="#9F5000">
+      <td><center><h2>Events</h2></center></td>
+	  <td><center><h2>Date</h2></center></td>
+	  <td><center><h2>Registration</h2></center></td>
+   </tr>
+   <tr bgcolor="#438787">
+      <td>
+	      <h3>Pragyan induction</h3>
+	  </td>
+	  <td>
+	     <center><b><h3>7-10th septenber</h3></b></center>
+	  </td>
+	  <td>
+	     <center><b><h3><a href="event.php">Register</a></h3></b></center>
+	  </td>
+   </tr>
+   <tr bgcolor="#438787">
+      <td>
+	      <h3>Genesis workshops</h3>
+	  </td>
+	  <td>
+	     <center><b><h3>13-15th September</h3></b></center>
+	  </td>
+	  <td>
+	     <center><b><h3><a href="event.php">Register</a></h3></b></center>
+	  </td>
+   </tr>
+   <tr bgcolor="#438787">
+      <td>
+	      <h3>Android app Developement</h3>
+	  </td>
+	  <td>
+	     <center><b><h3>28-31th Septenber</h3></b></center>
+	  </td>
+	  <td>
+	     <center><b><h3><a href="event.php">Register</a></h3></b></center>
+	  </td>
+   </tr>
+   <tr bgcolor="#438787">
+      <td>
+	      <h3>Joy of living week</h3>
+	  </td>
+	  <td>
+	     <center><b><h3>02-08th October</h3></b></center>
+	  </td>
+	  <td>
+	     <center><b><h3><a href="event.php">Register</a></h3></b></center>
+	  </td>
+   </tr>
+   <tr bgcolor="#438787">
+      <td>
+	      <h3>Horigons</h3>
+	  </td>
+	  <td>
+	     <center><b><h3>09-13th October</h3></b></center>
+	  </td>
+	  <td>
+	     <center><b><h3><a href="event.php">Register</a></h3></b></center>
+	  </td>
+   </tr>
+   <tr bgcolor="#438787">
+      <td>
+	      <h3>Lit week</h3>
+	  </td>
+	  <td>
+	     <center><b><h3>26th October</h3></b></center>
+	  </td>
+	  <td>
+	     <center><b><h3><a href="event.php">Register</a></h3></b></center>
+	  </td>
+   </tr>
+   <tr bgcolor="#438787">
+      <td>
+	      <h3>Manoranjani</h3>
+	  </td>
+	  <td>
+	     <center><b><h3>27-29th October</h3></b></center>
+	  </td>
+	  <td>
+	     <center><b><h3><a href="event.php">Register</a></h3></b></center>
+	  </td>
+   </tr>
+   <tr bgcolor="#438787">
+      <td>
+	      <h3>Spider Workshop</h3>
+	  </td>
+	  <td>
+	     <center><b><h3>30th October</h3></b></center>
+	  </td>
+	  <td>
+	     <center><b><h3><a href="event.php">Register</a></h3></b></center>
+	  </td>
+   </tr>
+</table>
+
+<footer>
+  <p>Posted by: Mukesh Mali</p>
+  <p>Contact information: <a href="contact.php">205112067@nitt.edu</a>.</p>
+<img src="img/ff.png" width="1499"></footer>
+
+</body>
+
+
+</html>
